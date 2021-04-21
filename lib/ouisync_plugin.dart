49bd7001d7f9db3ed35d933fc7c5ca4e3062fbde
Jorge Pabon <jmpabon@gmail.com>
2021-04-21 21:24:12 +0800

    Lookup implementatiohn for the function RegisterDart_PostCObject (in CallbackManager) Also: new class NAtiveCallback, and the static method for executing the function.

--- lib/ouisync_plugin.dart
+++ lib/ouisync_plugin.dart
@@ -10,4 +10,17 @@ final int Function(int x, int y) nativeTest =
     .lookup<NativeFunction<Int32 Function(Int32, Int32)>>('native_test')
     .asFunction();
 
+final nRegisterPostCObject = nativeOuiSyncLib.lookupFunction<
+  Void Function(
+    Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>>
+      functionPointer),
+  void Function(
+    Pointer<NativeFunction<Int8 Function(Int64, Pointer<Dart_CObject>)>>
+      functionPointer)>('RegisterDart_PostCObject');
 
+class NativeCallback {
+  static void setupNativeCallbacks() {
+    nRegisterPostCObject(NativeApi.postCObject);
+    print('Native callbacks setup ok');
+  }
+}
