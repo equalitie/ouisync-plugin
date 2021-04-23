524a8633dbcda5894b55c00d460342b56f632337
Jorge Pabon <jmpabon@gmail.com>
2021-04-23 12:55:40 +0800

    Added: localRepositories functions
    - initializeOuisyncRepository
    - createDir
    - getAttributes
    - readDir
    
    Note: code is commented until OuiSync library is compiled in the cmake file

--- lib/ouisync_plugin.dart
+++ lib/ouisync_plugin.dart
@@ -25,9 +25,73 @@ final nInitializeOuisyncRepository = nativeOuiSyncLib.lookupFunction<
   )
 >('initializeOuisyncRepository');
 
+final nReadDirAsync = nativeOuiSyncLib.lookupFunction<
+  Void Function(
+    Int64, 
+    Pointer<Utf8>,
+    Pointer<Utf8>
+  ),
+  void Function(
+    int, 
+    Pointer<Utf8>,
+    Pointer<Utf8>
+  )
+>('readDir');
+
+final nGetAttributesAsync = nativeOuiSyncLib.lookupFunction<
+  Void Function(
+    Int64,
+    Pointer<Utf8>,
+    Pointer<Pointer<Utf8>>,
+    Int32
+  ),
+  void Function(
+    int,
+    Pointer<Utf8>,
+    Pointer<Pointer<Utf8>>,
+    int
+  )
+>('getAttributes');
+
+final nCreateDirAsync = nativeOuiSyncLib.lookupFunction<
+  Int32 Function(
+    Int64,
+    Pointer<Utf8>,
+    Pointer<Utf8>
+  ),
+  int Function(
+    int,
+    Pointer<Utf8>,
+    Pointer<Utf8>
+  )
+>('createDir');
+
 class NativeCallback {
   static void setupNativeCallbacks() {
     nRegisterPostCObject(NativeApi.postCObject);
     print('Native callbacks setup ok');
+  }
+
+  static void initializeOuisyncRepository(String repoDir) async {
+    nInitializeOuisyncRepository.call(repoDir.toNativeUtf8());
+  } 
+
+  static Future<dynamic> createDirAsync(String repoPath, String newFolderPath) async {
+    return singleResponseFuture((port) => nCreateDirAsync.call(port.nativePort, repoPath.toNativeUtf8(), newFolderPath.toNativeUtf8()));
+  }
+
+  static Future<dynamic> getAttributesAsync(String repoPath, List<String> pathList) {
+    final Pointer<Pointer<Utf8>> pointerPathList = calloc(pathList.length);
+    final List<Pointer<Utf8>> utf8PathList = pathList.map((e) => e.toNativeUtf8()).toList();
+
+    for (var i = 0; i < pathList.length; i++) {
+      pointerPathList[i] = utf8PathList[i];
+    }
+
+    return singleResponseFuture((port) => nGetAttributesAsync.call(port.nativePort, repoPath.toNativeUtf8(), pointerPathList, pathList.length));
+  }
+
+  static Future<List<dynamic>> readDirAsync(String repoPath, String folderPath) async {
+    return singleResponseFuture((port) => nReadDirAsync(port.nativePort, repoPath.toNativeUtf8(), folderPath.toNativeUtf8()));
   }
 }
