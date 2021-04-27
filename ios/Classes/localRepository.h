927faca618c65073e3f47d4113718ada8260520c
Jorge Pabon <jmpabon@gmail.com>
2021-04-27 12:52:41 +0800

    Commented code for using the ouisync library was made available again, after adding the library compilation to the cmake file

--- ios/Classes/localRepository.h
+++ ios/Classes/localRepository.h
@@ -8,11 +8,11 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    // repo_dir is the repository identifier
-    void initializeOuisyncRepository(const char* repo_dir);
-    void createDir(Dart_Port callbackPort, const char* repo_dir, const char* c_new_directory);
-    void getAttributes(Dart_Port callbackPort, const char* repo_dir, const char  **c_paths, const int size);
-    void readDir(Dart_Port callbackPort, const char* repo_dir, const char* directory_to_read);
+      // repo_dir is the repository identifier
+      void initializeOuisyncRepository(const char* repo_dir);
+      void createDir(Dart_Port callbackPort, const char* repo_dir, const char* c_new_directory);
+      void getAttributes(Dart_Port callbackPort, const char* repo_dir, const char  **c_paths, const int size);
+      void readDir(Dart_Port callbackPort, const char* repo_dir, const char* directory_to_read);
 #ifdef __cplusplus
 }
 #endif
