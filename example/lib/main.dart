ccf8005208ae7504f701482e37ef279c145b1b3f
Adam Cigánek <adam.ciganek@gmail.com>
2021-05-13 16:37:20 +0200

    Add the example back to silence a warning

---
+++ example/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+//import 'package:ouisync_plugin/ouisync_plugin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Plugin example app'),
+        ),
+        body: Center(
+          child: Text('TODO'),
+        ),
+      ),
+    );
+  }
+}
