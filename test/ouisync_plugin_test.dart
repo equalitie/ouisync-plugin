65de483e933d04b8c1222588120bfd419d5790d5
Adam Cigánek <adam.ciganek@gmail.com>
2021-05-17 12:28:52 +0200

    Add minimal tests

--- test/ouisync_plugin_test.dart
+++ test/ouisync_plugin_test.dart
@@ -1,19 +1,53 @@
+import 'dart:convert';
 import 'package:test/test.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart';
 
 void main() {
-  Session? session;
+  late Session session;
+  late Repository repo;
 
   setUp(() async {
     session = await Session.open(':memory:');
+    repo = await Repository.open(session);
   });
 
   tearDown(() {
-    session!.close();
-    session = null;
+    repo.close();
+    session.close();
   });
 
-  test('stuff happens', () {
-    expect(false, equals(true));
+  test('file write and read', () async {
+    final path = '/test.txt';
+    final origContent = 'hello world';
+
+    {
+      final file = await File.create(repo, path);
+      await file.write(0, utf8.encode(origContent));
+      await file.close();
+    }
+
+    {
+      final file = await File.open(repo, path);
+
+      try {
+        final length = await file.length;
+        final readContent = utf8.decode(await file.read(0, length));
+
+        expect(readContent, equals(origContent));
+      } finally {
+        await file.close();
+      }
+    }
+  });
+
+  test('empty directory', () async {
+    final rootDir = await Directory.open(repo, '/');
+
+    try {
+      expect(rootDir, isEmpty);
+    } finally {
+      rootDir.close();
+    }
   });
 }
+
