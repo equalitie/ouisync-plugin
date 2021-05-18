33d440fb1ff836f39c2c377ae30a2cb318e89912
Adam Cigánek <adam.ciganek@gmail.com>
2021-05-18 09:27:03 +0200

    Expand the example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,19 +1,37 @@
 import 'package:flutter/material.dart';
-//import 'package:ouisync_plugin/ouisync_plugin.dart';
+import 'package:ouisync_plugin/ouisync_plugin.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
 
-void main() {
+void main() async {
   runApp(MyApp());
 }
 
 class MyApp extends StatefulWidget {
+  MyApp();
+
   @override
   _MyAppState createState() => _MyAppState();
 }
 
 class _MyAppState extends State<MyApp> {
+  Session? session;
+  Repository? repo;
+  Directory? dir;
+
   @override
   void initState() {
     super.initState();
+    load();
+  }
+
+  @override
+  void dispose() async {
+    dir?.close();
+    repo?.close();
+    session?.close();
+
+    super.dispose();
   }
 
   @override
@@ -24,9 +42,22 @@ class _MyAppState extends State<MyApp> {
           title: const Text('Plugin example app'),
         ),
         body: Center(
-          child: Text('TODO'),
+          child: Text('Num files in /: ${dir?.length ?? 0}')
         ),
       ),
     );
   }
+
+  void load() async {
+    final session = await Session.open(join((await getApplicationSupportDirectory()).path, 'db'));
+    final repo = await Repository.open(session);
+    final dir = await Directory.open(repo, '/');
+
+    setState(() {
+      this.session = session;
+      this.repo = repo;
+      this.dir = dir;
+    });
+  }
 }
+
