3d0420c66f155833747101c6eec148f2aa312299
Jorge Pabon <jmpabon@gmail.com>
2021-08-10 16:19:08 +0800

    ActivityAware interface implemented, to get the app main Activity, and use it for starting the share intent using the content provider.
    Also: shareFile (Dart --> Kotlin) method call implementation: receives the file path as an argument, which is used for create the Uri for the PipeProvider.
    The default code for  getting the platform version was removed.

--- android/src/main/kotlin/ie/equalit/ouisync_plugin/OuisyncPlugin.kt
+++ android/src/main/kotlin/ie/equalit/ouisync_plugin/OuisyncPlugin.kt
@@ -1,32 +1,79 @@
 package ie.equalit.ouisync_plugin
 
+import android.app.Activity
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import androidx.annotation.NonNull
 
+import io.flutter.embedding.engine.plugins.activity.ActivityAware
+import io.flutter.embedding.engine.plugins.activity.ActivityPluginBinding
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
-import io.flutter.plugin.common.PluginRegistry.Registrar
 
 /** OuisyncPlugin */
-class OuisyncPlugin: FlutterPlugin, MethodCallHandler {
+class OuisyncPlugin: FlutterPlugin, MethodCallHandler, ActivityAware {
   /// The MethodChannel that will the communication between Flutter and native Android
   ///
   /// This local reference serves to register the plugin with the Flutter Engine and unregister it
   /// when the Flutter Engine is detached from the Activity
-  private lateinit var channel : MethodChannel
+  var activity : Activity? = null
+  private lateinit var context : Context
+
+  companion object {
+    lateinit var channel : MethodChannel
+  }
+
+  override fun onAttachedToActivity(@NonNull activityPluginBinding: ActivityPluginBinding) {
+    print("onAttachedToActivity")
+    activity = activityPluginBinding.activity
+  }
+
+  override fun onDetachedFromActivityForConfigChanges() {
+    activity = null
+  }
+
+  override fun onReattachedToActivityForConfigChanges(@NonNull activityPluginBinding: ActivityPluginBinding) {
+    activity = activityPluginBinding.activity
+  }
+
+  override fun onDetachedFromActivity() {
+    activity = null
+  }
 
   override fun onAttachedToEngine(@NonNull flutterPluginBinding: FlutterPlugin.FlutterPluginBinding) {
     channel = MethodChannel(flutterPluginBinding.binaryMessenger, "ouisync_plugin")
     channel.setMethodCallHandler(this)
+
+    print("onAttachedToEngine")
+    context = flutterPluginBinding.getApplicationContext()
   }
 
-  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: Result) {
-    if (call.method == "getPlatformVersion") {
-      result.success("Android ${android.os.Build.VERSION.RELEASE}")
-    } else {
-      result.notImplemented()
+  override fun onMethodCall(@NonNull call: MethodCall, @NonNull result: MethodChannel.Result) {
+    when (call.method) {
+      "shareFile" -> {
+        val path = (call.arguments as HashMap<String, Any>)["path"]
+        val uri = Uri.parse(PipeProvider.CONTENT_URI.toString() + path)
+
+        var shareIntent = Intent()
+
+        shareIntent.action = Intent.ACTION_SEND
+        shareIntent.type = "*/*"
+
+        shareIntent.putExtra(Intent.EXTRA_STREAM, uri)
+        shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+
+        activity?.startActivity(Intent.createChooser(shareIntent, "Share file from OuiSync"))
+
+        result.success("Share file intent started")
+
+      }
+      else -> {
+        result.notImplemented()
+      }
     }
   }
 
