db284973a1918a6bd03223ff158a17ef4ffefec5
Jorge Pabon <jmpabon@gmail.com>
2021-12-07 17:59:10 +0800

    get_files_size_deadlock_test updated to use a file in the folder test/test_resources/ and write to a file using ChunkedStreamReader

--- test/get_files_size_deadlock_test.dart
+++ test/get_files_size_deadlock_test.dart
@@ -1,58 +1,39 @@
-import 'dart:convert';
+import 'dart:io' as io;
 
+import 'package:async/async.dart';
 import 'package:ouisync_plugin/ouisync_plugin.dart';
 import 'package:test/scaffolding.dart';
+import 'package:test/test.dart';
 
 void main() {
   late Session session;
   late Repository repository;
   late Subscription subscription;
 
-  String filesContent = 'Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.'
-  'Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.Lorem ipsum dolor sit amet.';
+  const String resourceFilePath = 'test/test_resources/simmel.pdf';
 
+  const String directoryPath = '/';
+  const String filePath = '/test_file.pdf';
 
-  Future<void> getDirectoryContents(Repository repo, String path) async {
+  const int bufferSize = 64000;
+
+  Future<void> syncDirectory(Repository repo, String path) async {
+    print('Syncing directory $path');
+    
     final folder1Contents = await Directory.open(repo, path);
-    print('Directory contents: ${folder1Contents.toList()}');
+    final contents = folder1Contents.toList();
     folder1Contents.close();
+
+    print('$path: $contents');
   }
 
   setUp(() async {
     session = await Session.open(':memory:');
     repository = await Repository.open(session, ':memory:');
 
-    subscription = repository.subscribe(() async {
-      print('Syncing /');
-      await getDirectoryContents(repository, '/');
-    });
+    subscription = repository.subscribe(() async =>
+      await syncDirectory(repository, directoryPath)
+    );
   });
 
   tearDown(() {
@@ -61,34 +42,99 @@ void main() {
     session.close();
   });
 
-  test('Create three files, open each file and request its length sequentially',
+  test('Create file, write to file (no chunks), then get file length',
   () async {
-    // Create file1.txt, file2.txt, file3.txt in root
-    {
-      for (var i = 0; i < 10; i++) {
-        final file = await File.create(repository, 'file$i.txt');
-        await file.write(0, utf8.encode(filesContent));
-        await file.close(); 
-        print('New file: file$i.txt');
-      }
 
-      for (var i = 0; i < 10; i++) {
-        final file = await File.open(repository, 'file$i.txt');
-        final length = await file.length;
-        file.close();
-        print('File file$i.txt size: $length');
-      }
+    // - Get stream from /test/test_resources/simmel.pdf,
+    // - Create new file /test_file.pdf
+    // - Write to file /test_file.pdf (no chunks)
+    // - Close/cancel file and stream reader
+    {
+      final testFile = io.File(resourceFilePath);
+      final bytes = await testFile.readAsBytes();
+    
+      try {
+        final file = await File.create(repository, filePath);
+        file.write(0, bytes.toList());
+        // file.write(0, utf8.encode(fileContents));
+        file.close(); 
+      } catch (e) {
+        print(e.toString());
+      } 
     }
 
-    // Get each file length sequentially
-    // {
-    //   for (var i = 0; i < 10; i++) {
-    //     final file = await File.open(repository, 'file$i.txt');
-    //     final length = await file.length;
-    //     file.close();
-    //     print('File file$i.txt size: $length');
-    //   }
-    // }
-  });
+    // Get file length for /test_file.pdf throws an error
+    {
+      print('Getting lenght of file $filePath');
+
+      int length = -1;
+      try {
+        final file = await File.open(repository, filePath);
+        length = await file.length;
+        file.close();
+
+        print('File $filePath length: $length');
+      } catch (e) {
+        print('${e.runtimeType}: ${e.toString()}');
+      }
+
+      expect(length, greaterThan(0));  
+    }
+  }, timeout: Timeout.none);
+
+  test('Create file, write to file in chunks from stream, then get file length',
+  () async {
+    // - Get stream from /test/test_resources/simmel.pdf,
+    // - Create new file /test_file.pdf
+    // - Write to file /test_file.pdf in chunks of bufferSize (64,000)
+    // - Close/cancel file and stream reader
+    {
+      final testFile = io.File(resourceFilePath);
+      final streamReader = ChunkedStreamReader(testFile.openRead());
+    
+      final file = await File.create(repository, filePath);
+      int offset = 0;
+      
+      try {
+        while (true) {
+          final buffer = await streamReader.readChunk(bufferSize);
+          print('Buffer size: ${buffer.length} - offset: $offset');
+
+          if (buffer.isEmpty) {
+            print('The buffer is empty; reading from the stream is done!');
+            break;
+          }
+
+          await file.write(offset, buffer);
+          offset += buffer.length;
+        }
+      } catch (e) {
+        print('Exception writing to file $filePath:\n${e.toString()}');
+      } finally {
+        streamReader.cancel();
+        file.close();
+
+        print('New file: $filePath created successfully');
+      }
+    }
+    
+    // Get file length for /test_file.pdf
+    {
+      print('Getting lenght of file $filePath');
+
+      int length = -1;
+      try {
+        final file = await File.open(repository, filePath);
+        length = await file.length;
+        file.close();
+
+        print('File $filePath length: $length');
+      } catch (e) {
+        print('${e.runtimeType}: ${e.toString()}');
+      }
+
+      expect(length, greaterThan(0));  
+    }
+  }, timeout: Timeout.none);
 
 }
