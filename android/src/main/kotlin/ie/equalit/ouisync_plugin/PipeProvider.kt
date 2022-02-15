49605f2a43e20adb5ef2f0edb0f0e2f0b243ff99
Adam Cigánek <adam.ciganek@gmail.com>
2022-02-15 11:24:10 +0100

    Fix the deadlock in PipeProvider by using a background handler thread

--- android/src/main/kotlin/ie/equalit/ouisync_plugin/PipeProvider.kt
+++ android/src/main/kotlin/ie/equalit/ouisync_plugin/PipeProvider.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.content.res.AssetFileDescriptor
 import android.net.Uri
 import android.os.Handler
+import android.os.HandlerThread
 import android.os.Looper
 import android.os.ParcelFileDescriptor
 import android.os.storage.StorageManager;
@@ -16,15 +17,25 @@ import java.io.InputStream
 import java.io.OutputStream
 import java.util.concurrent.Callable
 import java.util.concurrent.FutureTask
+import java.util.concurrent.Semaphore
 
 class PipeProvider: AbstractFileProvider() {
     companion object {
-        val TAG = javaClass.simpleName
         val CONTENT_URI: Uri = Uri.parse("content://ie.equalit.ouisync_plugin.pipe/")
-        const val CHUNK_SIZE = 64000
+        private const val CHUNK_SIZE = 64000
+        private val TAG = PipeProvider::class.java.simpleName
     }
 
+
+    private lateinit var workerThread: HandlerThread
+    private lateinit var workerHandler: Handler
+
     override fun onCreate(): Boolean {
+        workerThread = HandlerThread("${javaClass.simpleName} worker thread")
+        workerThread.start()
+
+        workerHandler = Handler(workerThread.getLooper())
+
         return true
     }
 
@@ -56,33 +67,36 @@ class PipeProvider: AbstractFileProvider() {
         // https://developer.android.google.cn/reference/android/os/storage/StorageManager
         return storage.openProxyFileDescriptor(
             ParcelFileDescriptor.MODE_READ_ONLY,
-            ProxyCallbacks(context!!, path, size),
-            Handler(context!!.mainLooper)
+            ProxyCallbacks(path, size),
+            workerHandler
         )
     }
 
     @Throws(FileNotFoundException::class)
     private fun openPipe(path: String): ParcelFileDescriptor? {
-        var pipe: Array<ParcelFileDescriptor?>?
+        TODO("Not yet implemented")
 
-        try {
-            pipe = ParcelFileDescriptor.createPipe()
-        } catch (e: IOException) {
-            Log.e(TAG, "Exception opening pipe", e)
-            throw FileNotFoundException("Could not open pipe for: " + path)
-        }
 
-        // pipe_id is only for debugging
-        var pipe_id = kotlin.random.Random.nextInt(Int.MAX_VALUE);
+        // var pipe: Array<ParcelFileDescriptor?>?
 
-        PipeTransfer(
-            pipe_id,
-            context!!,
-            path,
-            ParcelFileDescriptor.AutoCloseOutputStream(pipe[1])
-        ).start()
+        // try {
+        //     pipe = ParcelFileDescriptor.createPipe()
+        // } catch (e: IOException) {
+        //     Log.e(TAG, "Exception opening pipe", e)
+        //     throw FileNotFoundException("Could not open pipe for: " + path)
+        // }
 
-        return pipe[0]
+        // // pipe_id is only for debugging
+        // var pipe_id = kotlin.random.Random.nextInt(Int.MAX_VALUE);
+
+        // PipeTransfer(
+        //     pipe_id,
+        //     context!!,
+        //     path,
+        //     ParcelFileDescriptor.AutoCloseOutputStream(pipe[1])
+        // ).start()
+
+        // return pipe[0]
     }
 
     private fun getPathFromUri(uri: Uri): String {
@@ -101,124 +115,116 @@ class PipeProvider: AbstractFileProvider() {
     }
 
     internal class ProxyCallbacks(
-        private val context: Context,
         private val path: String,
         private val size: Long
     ) : android.os.ProxyFileDescriptorCallback() {
+        private val semaphore = Semaphore(1)
+
         override fun onGetSize() = size
 
-        override fun onRead(offset: Long, size: Int, ret_data: ByteArray): Int {
-            val semaphore = java.util.concurrent.Semaphore(1)
+        override fun onRead(offset: Long, chunkSize: Int, outData: ByteArray): Int {
             semaphore.acquire(1)
 
-            var ret_size: Int = 0
+            var outSize: Int = 0
 
-            // TODO: The handler here is never executed. I'm guessing it's because this
-            // onRead function is executed from inside the UiThread and because we also
-            // need to use the UiThread, we get a deadlock.
-            context.readChunkInUiThread(path, size, offset, object: MethodChannel.Result {
+            readChunkInUiThread(path, chunkSize, offset, object: MethodChannel.Result {
                 override fun success(a: Any?) {
                     val chunk = a as ByteArray
-                    ret_size = size;
-                    System.arraycopy(chunk, 0, ret_data, 0, size);
-                    semaphore.release(1);
+
+                    outSize = chunk.size
+                    chunk.copyInto(outData)
+                    semaphore.release(1)
                 }
 
-                override fun error(s0: String?, s1: String?, a: Any?) {
-                    Log.e(TAG, "error reading file (s0:$s0 s1:$s1 a:$a)")
-                    semaphore.release(1);
+                override fun error(errorCode: String?, errorMessage: String?, errorDetails: Any?) {
+                    Log.e(TAG, "error reading file at $path - code: $errorCode, message: $errorMessage details: $errorDetails")
+                    semaphore.release(1)
                 }
 
                 override fun notImplemented() {}
             })
 
-            Log.d(TAG, "Blocking on semaphore");
-            semaphore.acquire(1);
-            Log.d(TAG, "Semaphore released");
+            semaphore.acquire(1)
+            semaphore.release(1)
 
-            return ret_size
+            return outSize
         }
 
         override fun onRelease() {}
     }
 
-    internal class PipeTransfer(
-        private val pipe_id: Int,
-        private val context: Context,
-        private val path: String,
-        private var out: OutputStream
-    ) {
-        fun start() {
-            readChunk(0)
-        }
+    // internal class PipeTransfer(
+    //     private val pipe_id: Int,
+    //     private val context: Context,
+    //     private val path: String,
+    //     private var out: OutputStream
+    // ) {
+    //     fun start() {
+    //         readChunk(0)
+    //     }
 
-        private fun readChunk(offset: Long) {
-            context.readChunkInUiThread(path, PipeProvider.CHUNK_SIZE, offset, object : MethodChannel.Result {
-                override fun success(a: Any?) {
-                    val chunk = a as ByteArray
+    //     private fun readChunk(offset: Long) {
+    //         readChunkInUiThread(path, PipeProvider.CHUNK_SIZE, offset, object : MethodChannel.Result {
+    //             override fun success(a: Any?) {
+    //                 val chunk = a as ByteArray
 
-                    // We're currently not in our custom thread, and the
-                    // writeChunk function is blocking, so we need to spawn a
-                    // new thread so as to not block the whole app. This is a
-                    // temporary quick fix and should be dealt with better.
-                    Thread {
-                        writeChunk(chunk, offset)
-                    }.start()
-                }
+    //                 // We're currently not in our custom thread, and the
+    //                 // writeChunk function is blocking, so we need to spawn a
+    //                 // new thread so as to not block the whole app. This is a
+    //                 // temporary quick fix and should be dealt with better.
+    //                 Thread {
+    //                     writeChunk(chunk, offset)
+    //                 }.start()
+    //             }
 
-                override fun error(s0: String?, s1: String?, a: Any?) {
-                    Log.e(TAG, "$pipe_id: error reading file (s0:$s0 s1:$s1 a:$a)")
-                    out.close();
-                }
+    //             override fun error(s0: String?, s1: String?, a: Any?) {
+    //                 Log.e(TAG, "$pipe_id: error reading file (s0:$s0 s1:$s1 a:$a)")
+    //                 out.close();
+    //             }
 
-                override fun notImplemented() {}
-            })
-        }
+    //             override fun notImplemented() {}
+    //         })
+    //     }
 
-        private fun writeChunk(chunk: ByteArray, offset: Long) {
-            if (chunk.isNotEmpty()) {
-                Log.d(TAG, "$pipe_id: Chunk received. size: ${chunk.size} offset: $offset")
+    //     private fun writeChunk(chunk: ByteArray, offset: Long) {
+    //         if (chunk.isNotEmpty()) {
+    //             Log.d(TAG, "$pipe_id: Chunk received. size: ${chunk.size} offset: $offset")
 
-                try {
-                    out.write(chunk, 0, chunk.size)
-                } catch (e: IOException) {
-                    Log.e(TAG, "$pipe_id: Exception writing to pipe", e)
-                    // TODO: Not 100% sure about this one. Without it I saw
-                    // messages about resources not being closed, but I haven't
-                    // really seen any examples do such explicit closing.
-                    out.close()
-                    return;
-                }
+    //             try {
+    //                 out.write(chunk, 0, chunk.size)
+    //             } catch (e: IOException) {
+    //                 Log.e(TAG, "$pipe_id: Exception writing to pipe", e)
+    //                 // TODO: Not 100% sure about this one. Without it I saw
+    //                 // messages about resources not being closed, but I haven't
+    //                 // really seen any examples do such explicit closing.
+    //                 out.close()
+    //                 return;
+    //             }
 
-                readChunk(offset + chunk.size)
-            }
+    //             readChunk(offset + chunk.size)
+    //         }
 
-            if (chunk.isEmpty()) {
-                Log.d(TAG, "$pipe_id: Chunk empty, closing OutputStream")
+    //         if (chunk.isEmpty()) {
+    //             Log.d(TAG, "$pipe_id: Chunk empty, closing OutputStream")
 
-                out.flush()
-                out.close()
-            }
-        }
+    //             out.flush()
+    //             out.close()
+    //         }
+    //     }
+    // }
+}
+
+private fun readChunkInUiThread(path: String, chunkSize: Int, offset: Long, callbacks: MethodChannel.Result) {
+    Handler(Looper.getMainLooper()).post {
+        readChunk(path, chunkSize, offset, callbacks)
     }
 }
 
-private fun Context.readChunkInUiThread(path: String, size: Int, offset: Long, callbacks: MethodChannel.Result) {
-    var func = {
-        val arguments = HashMap<String, Any>()
+private fun readChunk(path: String, chunkSize: Int, offset: Long, callbacks: MethodChannel.Result) {
+    val arguments = hashMapOf<String, Any>("path"      to path,
+                                           "chunkSize" to chunkSize,
+                                           "offset"    to offset)
 
-        arguments["path"] = path
-        arguments["chunkSize"] = size
-        arguments["offset"] = offset
-
-        OuisyncPlugin.channel.invokeMethod("readOuiSyncFile", arguments, callbacks)
-    }
-
-    if (Looper.myLooper() == Looper.getMainLooper()) {
-        func.invoke()
-    } else {
-        Handler(this.mainLooper).post {
-            func.invoke()
-        }
-    }
+    OuisyncPlugin.channel.invokeMethod("readOuiSyncFile", arguments, callbacks)
 }
+
