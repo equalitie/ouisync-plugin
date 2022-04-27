a23ecc9ddc688d0e8f50b907353ebc7a72cf6c20
J-Pabon <jmpabon@gmail.com>
2022-04-27 16:37:49 +0800

    Added:  test for issue when closing a repository asynchronously in Windows

---
+++ test/close_repo_async_test.dart
@@ -0,0 +1,29 @@
+import 'dart:io' as io;
+import 'package:test/test.dart';
+import 'package:ouisync_plugin/ouisync_plugin.dart';
+
+void main() {
+  late io.Directory temp;
+  late Session session;
+  late Repository repo;
+
+  setUp(() async {
+    temp = await io.Directory.systemTemp.createTemp();
+    session = await Session.open('${temp.path}/config');
+    repo = await Repository.create(session,
+        store: ':memory:', password: 'test123');
+  });
+
+  tearDown(() {
+    session.close();
+    temp.deleteSync;
+  });
+
+  test('Close a repository asynchronously in Windows fails', () async {
+    await repo.close();
+  });
+
+  // test('Close a repository asynchronously  in Windows using microtask succeeds', () {
+  //   Future.microtask(() async => await repo.close());
+  // });
+}
